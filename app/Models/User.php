74412407489a11771953dfd43c614db2e96b609e
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-12-09 10:16:59 +0800

    implement uuid in model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,34 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    protected static function boot() {
+        static::creating(function ($model) {
+            if ( ! $model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+     /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +49,8 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    
 
     /**
      * The attributes that should be hidden for arrays.
