d3920dc72ec6a2f29f33c612a0dd72562450f38d
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-12-10 10:47:17 +0800

    registering middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\emailVerified;
+use App\Http\Middleware\notAdmin;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/route-1', function () {
+    //
+})->middleware(emailVerified::class);
+
+Route::get('/route-2', function () {
+    //
+})->middleware(notAdmin::class);
