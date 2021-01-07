11adee04a542ae064391c71ed3b2ac41b85f7df3
AndyAlyf <andyalyfsyah4@gmail.com>
2021-01-07 17:15:41 +0800

    add state

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import router from './router.js'
 import App from './App.vue'
 import vuetify from './vuetify.js'
+import store from './store.js' 
 import './bootstrap.js'
 
 Vue.component('campaign-item', require('./components/CampaignItem.vue').default);
@@ -9,6 +10,7 @@ Vue.component('campaign-item', require('./components/CampaignItem.vue').default)
 const app = new Vue({
     el: "app",
     router,
+    store,
     vuetify,
     components: {
         App
